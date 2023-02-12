5299bd03c72c1c591432d3c621e783e8729f7226
Akira Masuda <masuda.akira1014@gmail.com>
2023-02-12 22:15:41 +0900

    support excel import to mysql

--- cmd/db-setup/main.go
+++ cmd/db-setup/main.go
@@ -9,17 +9,11 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	db, err := sql.Open("mysql", "root:rootpassword@(localhost:3306)/local")
-	if err != nil {
-		panic(err)
+func runSQLDump(db *sql.DB, path string) error {
+	if _, err := os.Stat(path); err != nil {
+		return err
 	}
-	defer func() {
-		if err = db.Close(); err != nil {
-			panic(err)
-		}
-	}()
-	b, err := os.ReadFile("./db/schema/jira_tables.sql")
+	b, err := os.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
@@ -45,5 +39,54 @@ func main() {
 		// sql continues to next line
 		query += line
 	}
-	fmt.Println("success")
+	return nil
+}
+
+func setupTables(db *sql.DB) error {
+	if err := runSQLDump(db, "./db/schema/jira_tables.sql"); err != nil {
+		return fmt.Errorf("setup jira table: %w", err)
+	}
+	return nil
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:rootpassword@(localhost:3306)/")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = db.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	if err := runSQLDump(db, "./db/schema/databases.sql"); err != nil {
+		panic(fmt.Errorf("setup database: %w", err))
+	}
+	// setup database for production
+	pmdb, err := sql.Open("mysql", "root:rootpassword@(localhost:3306)/PM")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = pmdb.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	if err := setupTables(pmdb); err != nil {
+		panic(err)
+	}
+	// setup database for tests
+	pmtestdb, err := sql.Open("mysql", "root:rootpassword@(localhost:3306)/PM_TEST")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = pmtestdb.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	if err := setupTables(pmtestdb); err != nil {
+		panic(err)
+	}
+	fmt.Println("db setup success")
 }
